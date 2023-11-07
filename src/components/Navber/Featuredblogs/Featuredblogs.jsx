eca4fa2fe96757239f44e8a511293db6ba5c16d3
robiul704 <robiulislam29935@gmail.com>
2023-11-08 00:46:03 +0600

    featured section sorting work set successfully

--- src/components/Navber/Featuredblogs/Featuredblogs.jsx
+++ src/components/Navber/Featuredblogs/Featuredblogs.jsx
@@ -1,6 +1,19 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Featuredblogs = () => {
+    const [blogs,setBlogs]=useState([])
+    console.log(blogs)
+
+    useEffect(() => {
+        
+        axios.get('http://localhost:5000/blogs')
+        .then((response) => {
+          const sortedBlogs = response.data.sort((a, b) => b.longdescription.split(' ').length - a.longdescription.split(' ').length);
+          const top10Blogs = sortedBlogs.slice(0, 10);
+          setBlogs(top10Blogs);
+        });
+      }, []);
     return (
         <div>
             featured blogs
