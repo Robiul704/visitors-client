ca94d153d62d3f8ef710c9b031d183b2bb0a8a0b
robiul704 <robiulislam29935@gmail.com>
2023-11-08 23:13:15 +0600

    gallery section is added

--- src/components/Home/Seemore.jsx
+++ src/components/Home/Seemore.jsx
@@ -7,16 +7,16 @@ const Seemore = () => {
 <h1 className="text-2xl font-bold">How to choose the best blogging platform</h1>
 While all platforms slightly vary in terms of features, the best blogging platforms should cover all core features including an easy-to-use content editor, tons of themes and templates, some search engine optimization SEO tools (or an ability to install third-party ones), and an ample app store with extensions or integrations with third-party tools. 
 
-In addition to this, you should look for advanced security, analytics and performance tracking tools, a solid customer service, ecommerce features (if you plan to monetize your blog), and a free trial (or at least a money-back guarantee) – so you can try out the platform before spending any money. 
+In addition to this,your should look for advanced security, analytics and performance tracking tools, a solid customer service, ecommerce features (if you plan to monetize your blog), and a free trial (or at least a money-back guarantee) – so you can try out the platform before spending any money. 
 
-Also, before opting for one blogging platform over the others, you should have an idea of how much creative freedom and control you wish to have over your blog – as well as what level of technical know-how you possess. 
+Also,before opting for one blogging platform over the others, you should have an idea of how much creative freedom and control you wish to have over your blog – as well as what level of technical know-how you possess. 
 
 <h1 className="text-2xl font-bold mt-5">How we test the best blogging platforms</h1>
 Firstly, we’ll check what plans are offered by the provider, how many tiers there are, and evaluate their main and unique features while paying special attention to the value that out-of-the-box features offer in comparison to those available as add-ons.
 
 Then we’ll test for ourselves how simple it is to purchase a plan with a provider, set up a blog, and start adding content to it – with an emphasis on ease of use, functionality, and flexibility.
 
-In the end, we’ll estimate what value for money the provider offers and how do their products compare with similar blogging platforms.
+In the end,we’ll estimate what value for money the provider offers and how do their products compare with similar blogging platforms.
 
 Read how we test, rate, and review products on TechRadar.
 
