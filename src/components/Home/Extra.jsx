33cdf2d88ddcbdfb78b094d6f204073fa1f8c154
robiul704 <robiulislam29935@gmail.com>
2023-11-08 23:11:51 +0600

    extra 1 section is added

--- src/components/Home/Extra.jsx
+++ src/components/Home/Extra.jsx
@@ -3,7 +3,10 @@
 const Extra = () => {
     return (
         <div className="my-10">
-            <h1 className="font-bold text-5xl text-center my-5">Gallery</h1>
+
+
+
+            <h1 className="font-bold text-5xl text-center my-6">Gallery</h1>
             <div className="grid lg:grid-cols-4 gap-5">
             <img className="h-56 w-full" src="https://i.ibb.co/zbf2mKp/niagara-falls-3510870-640.jpg" alt="" />
             <img className="h-56 w-full" src="https://i.ibb.co/yBYRb2L/download-8.jpg" alt="" />
