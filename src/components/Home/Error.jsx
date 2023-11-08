79824725c38bdb5e0e973d936b946aaefe92edc3
robiul704 <robiulislam29935@gmail.com>
2023-11-08 23:14:57 +0600

    404 PAGE is add and there have a home btn

--- src/components/Home/Error.jsx
+++ src/components/Home/Error.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Error = () => {
     return (
         <div className="text-4xl font-extrabold text-center container mx-auto">
-        <h1 className="text-4xl font-extrabold text-center">404 there is no data found</h1>
+        <h1 className="text-4xl font-extrabold text-center">404 There is no data found</h1>
         <NavLink className="text-4xl font-extrabold text-center" to={'/'}><button className="btn">Home</button></NavLink>
     </div>
     );
