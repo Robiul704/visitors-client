1aad32fbb91a6a7562bcb1e54958fdb0cc1ce761
robiul704 <robiulislam29935@gmail.com>
2023-11-08 23:14:13 +0600

    banner section is add using image bb

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
   <div className="hero-content text-center text-neutral-content">
     <div className="items-center grid justify-around gap-10 lg:grid-cols-2">
       <img className=" min-h-screen" src="https://i.ibb.co/6ZW8xCf/blog-icon.png" alt="" />
-      <p className="mb-5 text-3xl font-bold ">The best blogging platforms make it simple and easy for you to set up and manage a blog, whether for personal or business use.we make sure the news of sports, education, health and nature.you can explore this types of blogs that we provide for you</p>
+      <p className="mb-6 text-3xl font-bold ">The best blogging platforms make it simple and easy for you to set up and manage a blog, whether for personal or business use.we make sure the news of sports, education, health and nature.you can explore this types of blogs that we provide for you</p>
       
     </div>
   </div>
