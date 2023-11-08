a1f01d6c267f3380786fb845a11531fc80112bf7
robiul704 <robiulislam29935@gmail.com>
2023-11-08 23:11:08 +0600

    recent blogs work is finished

--- src/components/Home/Resentblogs.jsx
+++ src/components/Home/Resentblogs.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Blogss from "./Blogss";
-
-
 const Resentblogs = () => {
     const [blogs,setblogs]=useState([])
     console.log(blogs)
@@ -22,7 +20,6 @@ const Resentblogs = () => {
              <h1 className="text-5xl font-bold text-center my-5 pt-5">Resent Blogs</h1>
            <div className="grid grid-cols-2 gap-10 my-10 justify-center">
            
-          
           {
             blogs.map(blog=><Blogss key={blog._id} blog={blog}></Blogss>)
            }
